78c0686a50b97eeceacd7e31477b800e3cc8aa75
Jason <jason@hedmans.org>
2025-10-04 15:09:34 -0400

    memoize disable flag

--- packages/app/control/src/app/(app)/app/[id]/settings/_components/form/button.tsx
+++ packages/app/control/src/app/(app)/app/[id]/settings/_components/form/button.tsx
@@ -3,12 +3,17 @@
 import { useFormState } from 'react-hook-form';
 
 import { Button } from '@/components/ui/button';
+import { useMemo } from 'react';
 
 export const FormButton = () => {
   const form = useFormState();
 
+  const disabled = useMemo(() => {
+    return !form.isValid || !form.isDirty;
+  }, [form.isValid, form.isDirty]);
+
   return (
-    <Button type="submit" disabled={!form.isValid || !form.isDirty}>
+    <Button type="submit" disabled={disabled}>
       Save
     </Button>
   );
